e80be8dbb907f98f3361a1cd63077e90c154e684
Louwrens Labuschagne <louwjlabuschagne@gmail.com>
2019-08-27 15:49:36 +0200

    Train Clasisifer part 1 port from notebook

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,25 +1,59 @@
 import sys
+from sqlalchemy import create_engine
+import pandas as pd
 
 
 def load_data(database_filepath):
-    pass
+    # load data from database
+    engine = create_engine('sqlite:///' + database_filepath)
+    with engine.begin() as conn:
+        df = pd.read_sql('SELECT * FROM Message', con=conn)
+    X = df.message
+    Y = df.drop(['index', 'id', 'message',
+                 'original', 'genre', 'index'], axis=1)
+
+    return X, Y, Y.columns
 
 
 def tokenize(text):
-    pass
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
     # text processing and model pipeline
 
-
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
     # define parameters for GridSearchCV
+    parameters = {'vect__ngram_range': ((1, 1), (1, 2)),
+            # 'vect__max_df': (0.5, 0.75, 1.0),
+            # 'vect__max_features': (None, 5000, 10000),
+            # 'tfidf__use_idf': (True, False),
+            # 'clf__estimator__n_estimators': [50, 100, 200],
+            'clf__estimator__min_samples_split': [2, 3, 4]}
 
-
+    cv = GridSearchCV(pipeline, param_grid=parameters, n_jobs=4)
     # create gridsearch object and return as final model pipeline
 
-    #return model_pipeline
-    pass
+    # return model_pipeline
+    pass cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
@@ -35,7 +69,8 @@ def main():
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        X_train, X_test, Y_train, Y_test = train_test_split(
+            X, Y, test_size=0.2)
 
         print('Building model...')
         model = build_model()
@@ -52,9 +87,9 @@ def main():
         print('Trained model saved!')
 
     else:
-        print('Please provide the filepath of the disaster messages database '\
-              'as the first argument and the filepath of the pickle file to '\
-              'save the model to as the second argument. \n\nExample: python '\
+        print('Please provide the filepath of the disaster messages database '
+              'as the first argument and the filepath of the pickle file to '
+              'save the model to as the second argument. \n\nExample: python '
               'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
 
 
